09c2203e2b1594ce416fe298258f801aff852a2c
Rushabh Shiradkar <rushabhshiradkar1717@gmail.com>
2025-12-17 19:33:28 +0530

    basic errors solved

--- backend/src/main/java/com/fakejobdetection/security/SecurityConfig.java
+++ backend/src/main/java/com/fakejobdetection/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.fakejobdetection.config;
+package com.fakejobdetection.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
